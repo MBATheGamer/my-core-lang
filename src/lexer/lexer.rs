19b8366f32508909a622b1a72d1544b43260fe80
MBATheGamer <mbathegamer@gmail.com>
2024-02-05 23:30:34 +0100

    Add read_char to Lexer

--- src/lexer/lexer.rs
+++ src/lexer/lexer.rs
@@ -7,13 +7,26 @@ pub struct Lexer {
 
 impl Lexer {
   pub fn new(input: &str) -> Lexer {
-    let lexer = Lexer {
+    let mut lexer = Lexer {
       input: input.chars().collect(),
       position: 0,
       read_position: 0,
       ch: Default::default()
     };
 
+    lexer.read_char();
+
     return lexer;
+  }
+
+  fn read_char(&mut self) {
+    if self.read_position >= self.input.len() {
+      self.ch = '\0';
+    } else {
+      self.ch = self.input[self.read_position];
+    }
+
+    self.position = self.read_position;
+    self.read_position += 1;
   }
 }
